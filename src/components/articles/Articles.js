87aad04109c031f07c48e44ec868b369cbad1acd
Andrea-March <andrea.marzohv@gmail.com>
2021-11-30 19:37:05 +0100

    added carousel for articles

--- src/components/articles/Articles.js
+++ src/components/articles/Articles.js
@@ -1,63 +1,13 @@
-import React, {useState} from "react";
-import ArticleCard from "./article-card/ArticleCard";
-import v60 from '../../static/v60/v60-ok2.jpg'
-import french from '../../static/french.jpg'
-import cappuccino from '../../static/cappuccino.jpg'
+import React from "react";
 import './articles.css'
-import {useNavigate} from "react-router-dom";
+import ArticlesCarousel from "./ArticlesCarousel";
 
 const Articles = () => {
 
-    const navigate = useNavigate()
 
-    const [currentIndex, setIndex] = useState(0)
-
-
-    const handleProceed = (articleId) => {
-        console.log(articleId)
-        articleId && navigate(`/articles/${articleId}`);
-    };
-
-    let articleList = [
-        {
-            id: 1,
-            title: 'The Art of V60',
-            date: '2021.11.26',
-            thumb: v60
-        },{
-            id: 2,
-            title: 'The Perfect Frech Press',
-            date: '2021.11.26',
-            thumb: french
-        },{
-            id: 3,
-            title: 'How to make latte at home',
-            date: '2021.11.26',
-            thumb: cappuccino
-        }
-    ]
-
-    const getPrevious = (currentIndex) => {
-        return currentIndex - 1 < 0 ? articleList.length - 1 : currentIndex - 1
-    }
-    const getNext = (currentIndex) => {
-        return currentIndex + 1 >= articleList.length ? 0 : currentIndex + 1
-    }
-    const slideLeft = () => {
-        setIndex(getPrevious(currentIndex))
-    }
-    const slideRight = () => {
-        setIndex(getNext(currentIndex))
-    }
     return(
         <div className="flex-column">
-            <div className="cover-articles">
-                <ArticleCard article={articleList[getPrevious(currentIndex)]} position={"left"} onClick={()=>{}}/>
-                <i className="fa fa-arrow-left fa-2x" onClick={slideLeft}/>
-                <ArticleCard article={articleList[currentIndex]} position={"center"} onClick={handleProceed}/>
-                <i className="fa fa-arrow-right fa-2x" onClick={slideRight}/>
-                <ArticleCard article={articleList[getNext(currentIndex)]} position={"right"} onClick={()=>{}}/>
-            </div>
+          <ArticlesCarousel />
             <div className="shortcut-images">
                 <div className="powders shortcut">
                     Powders
--- src/components/articles/ArticlesCarousel.js
+++ src/components/articles/ArticlesCarousel.js
@@ -1,14 +1,54 @@
-import React from "react";
+import React, {useState} from "react";
 import ArticleCard from "./article-card/ArticleCard";
+import {useNavigate} from "react-router-dom";
+import v60 from "../../static/v60/v60-ok2.jpg";
+import french from "../../static/french.jpg";
+import cappuccino from "../../static/cappuccino.jpg";
+import './ArticlesCarousel.css'
 
-const ArticlesCarousel = ({articlesList, slideLeft, slideRight}) => {
+const ArticlesCarousel = () => {
+    const navigate = useNavigate()
+    const [currentIndex, setIndex] = useState(0)
+    let articleList = [
+        {
+            id: 1,
+            title: 'The Art of V60',
+            date: '2021.11.26',
+            thumb: v60
+        },{
+            id: 2,
+            title: 'The Perfect Frech Press',
+            date: '2021.11.26',
+            thumb: french
+        },{
+            id: 3,
+            title: 'How to make latte at home',
+            date: '2021.11.26',
+            thumb: cappuccino
+        }
+    ]
+    const handleProceed = (articleId) => {
+        articleId && navigate(`/articles/${articleId}`);
+    };
+    const getPrevious = (currentIndex) => {
+        return currentIndex - 1 < 0 ? articleList.length - 1 : currentIndex - 1
+    }
+    const getNext = (currentIndex) => {
+        return currentIndex + 1 >= articleList.length ? 0 : currentIndex + 1
+    }
+    const slideLeft = () => {
+        setIndex(getPrevious(currentIndex))
+    }
+    const slideRight = () => {
+        setIndex(getNext(currentIndex))
+    }
     return(
         <div className="cover-articles">
-            <ArticleCard article={articlesList[0]} position={"left"}/>
-            <i className="fa fa-arrow-left fa-2x" onClick={slideLeft}/>
-            <ArticleCard article={articlesList[1]} position={"center"}/>
-            <i className="fa fa-arrow-right fa-2x" onClick={slideRight}/>
-            <ArticleCard article={articlesList[2]} position={"right"}/>
+            <ArticleCard article={articleList[getPrevious(currentIndex)]} position={"left"} onClick={()=>{}}/>
+            <i className="fa fa-angle-left fa-3x" onClick={slideLeft}/>
+            <ArticleCard article={articleList[currentIndex]} position={"center"} onClick={handleProceed}/>
+            <i className="fa fa-angle-right fa-3x" onClick={slideRight}/>
+            <ArticleCard article={articleList[getNext(currentIndex)]} position={"right"} onClick={()=>{}}/>
         </div>
     )
 }
