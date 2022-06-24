f47a4ced933ca3456933fcf33df929ae0fadac44
Andrea-March <andrea.marzohv@gmail.com>
2022-06-24 17:31:47 +0200

    changed article mock

--- src/mock/Articles.js
+++ src/mock/Articles.js
@@ -37,90 +37,95 @@ const ArticleList = [
     }
 ]
 
-const articleBody = " <p>\n" +
-    "                        While it is true that a good coffee is obtained with an excellent extraction, it is equally true that how we have preserved our coffee is essential to obtain a drink with strong tastes and aromas.\n" +
-    "                        Let's see what are the tricks to better preserve our coffee, and what are the mistakes to be avoided absolutely.\n" +
-    "                    </p>\n" +
-    "                    <h4>The perfect time for grinding</h4>\n" +
-    "                    <p>\n" +
-    "                        There is a big difference in storage time between the whole bean and a ground coffee.\n" +
-    "                        In fact, the whole bean still retains its intense flavors and aromas for about a month after roasting, while the ground for at most 2 weeks!\n" +
-    "                        This is because after grinding, a larger surface is exposed to the air, which through the oxidation process will quickly spoil our favorite coffee.\n" +
-    "                    </p>\n" +
-    "                    <span>\n" +
+const articleBody = " <>\n" +
+    "        <p>\n" +
+    "            While it is true that a good coffee is obtained with an excellent extraction, it is equally true that how we have preserved our coffee is essential to obtain a drink with strong tastes and aromas.\n" +
+    "            Let's see what are the tricks to better preserve our coffee, and what are the mistakes to be avoided absolutely.\n" +
+    "        </p>\n" +
+    "    <h4>The perfect time for grinding</h4>\n" +
+    "            <img alt=\"grinding coffee\" loading=\"lazy\" src=\"https://drive.google.com/uc?id=1oHiz0mBtorG1_sCrrJlYUDjyLHqFvLdu\"/>\n" +
+    "    <p>\n" +
+    "        There is a big difference in storage time between the whole bean and a ground coffee.\n" +
+    "        In fact, the whole bean still retains its intense flavors and aromas for about a month after roasting, while the ground for at most 2 weeks!\n" +
+    "        This is because after grinding, a larger surface is exposed to the air, which through the oxidation process will quickly spoil our favorite coffee.\n" +
+    "    </p>\n" +
+    "    <span id=\"center-span\">\n" +
     "                        To prevent this from happening, and to make sure you fully savor all the hidden flavors, it is best to grind your coffee just before using it.\n" +
     "                        </span>\n" +
     "\n" +
-    "                    <p>\n" +
-    "                        For this reason, a heavy coffee drinker should consider making a preparation and buying a manual grinder (more affordable) or an electric one (typically more expensive).\n" +
-    "                        <br/>\n" +
-    "                        Also, to make sure you always have new coffee, we recommend buying small quantities on a regular basis.\n" +
-    "                        This prevents part of the large packet of coffee we bought from being left unused for a long time and thus losing its flavors.\n" +
-    "                    </p>\n" +
-    "                    <h4>Avoid Air, Light, Heat and Moisture!</h4>\n" +
-    "                    <p>\n" +
-    "                        Even if we cannot always grind our coffee immediately before using it, we can certainly be careful about its conservation, even if we have bought it already ground.\n" +
-    "                        As we have already learned, air is one of the main enemies of coffee, as oxidation accelerates its stale process.\n" +
-    "                        For this reason it is necessary to keep the coffee sealed and with as little air as possible in the container.\n" +
-    "                    </p>\n" +
-    "                    <span>\n" +
+    "    <p>\n" +
+    "        For this reason, a heavy coffee drinker should consider making a preparation and buying a manual grinder (more affordable) or an electric one (typically more expensive).\n" +
+    "        <br/>\n" +
+    "        Also, to make sure you always have new coffee, we recommend buying small quantities on a regular basis.\n" +
+    "        This prevents part of the large packet of coffee we bought from being left unused for a long time and thus losing its flavors.\n" +
+    "    </p>\n" +
+    "    <h4>Avoid Air, Light, Heat and Moisture!</h4>\n" +
+    "            <img alt=\"grinding coffee\" loading=\"lazy\" src=\"https://drive.google.com/uc?id=1MTbXRv_T5H5d_9H7Ov-ji1m3nlWaazKJ\"/>\n" +
+    "    <p>\n" +
+    "        Even if we cannot always grind our coffee immediately before using it, we can certainly be careful about its conservation, even if we have bought it already ground.\n" +
+    "        As we have already learned, air is one of the main enemies of coffee, as oxidation accelerates its stale process.\n" +
+    "        For this reason it is necessary to keep the coffee sealed and with as little air as possible in the container.\n" +
+    "    </p>\n" +
+    "    <span id=\"center-span\">\n" +
     "                        Coffee is sometimes sold in ziplock bags, and this is a great way to make sure no air can get in once the container is sealed.\n" +
     "                    </span>\n" +
-    "                    <p>\n" +
-    "                        In addition, when the ziplock is present, there is also a small valve from which we can let out as much air as we can once the container has been closed.\n" +
-    "                    </p>\n" +
-    "                    <span>\n" +
+    "    <p>\n" +
+    "        In addition, when the ziplock is present, there is also a small valve from which we can let out as much air as we can once the container has been closed.\n" +
+    "    </p>\n" +
+    "    <span id=\"center-span\">\n" +
     "                        An alternative is to pour the coffee into an airtight container as soon as possible,\n" +
     "                    </span>\n" +
-    "                    <p>\n" +
-    "                        and try to let out as much air as possible to reduce the amount of oxygen our precious coffee is in contact with.\n" +
-    "                    </p>\n" +
-    "                    <p>\n" +
-    "                        In addition to oxygen, direct exposure to sunlight also contributes to making our coffee stale faster, due to a process called 'photodegradation'\n" +
-    "                        for this reason\n" +
-    "                    </p>\n" +
-    "                    <span>\n" +
+    "    <p>\n" +
+    "        and try to let out as much air as possible to reduce the amount of oxygen our precious coffee is in contact with.\n" +
+    "    </p>\n" +
+    "    <p>\n" +
+    "        In addition to oxygen, direct exposure to sunlight also contributes to making our coffee stale faster, due to a process called 'photodegradation'\n" +
+    "        for this reason\n" +
+    "    </p>\n" +
+    "    <span id=\"center-span\">\n" +
     "                        Our container should be opaque\n" +
     "                    </span>\n" +
-    "                    <p>\n" +
-    "                        so as not to allow direct light to interact with our coffee. If we don't have an opaque container, don't worry!\n" +
-    "                        It will be sufficient to store it in a place not exposed to direct sunlight.\n" +
-    "                    </p>\n" +
-    "                    <p>\n" +
-    "                        the last enemy of coffee is humidity. Our whole beans and ground coffee attract moisture, but this isn't good for the end result at all.\n" +
-    "                        For this reason it is necessary to keep the coffee in a dry place and\n" +
-    "                    </p>\n" +
-    "                    <span>\n" +
+    "    <p>\n" +
+    "        so as not to allow direct light to interact with our coffee. If we don't have an opaque container, don't worry!\n" +
+    "        It will be sufficient to store it in a place not exposed to direct sunlight.\n" +
+    "    </p>\n" +
+    "    <p>\n" +
+    "        the last enemy of coffee is humidity. Our whole beans and ground coffee attract moisture, but this isn't good for the end result at all.\n" +
+    "        For this reason it is necessary to keep the coffee in a dry place and\n" +
+    "    </p>\n" +
+    "    <span id=\"center-span\">\n" +
     "                        Absolutely avoid the refrigerator or freezer!\n" +
     "                    </span>\n" +
-    "                    <p>\n" +
-    "                        In fact, the continuous change of temperature when taking the coffee and putting it back in the cold, causes the appearance of condensation,\n" +
-    "                        which is absorbed by the coffee, ruining the aromas.\n" +
-    "                    </p>\n" +
-    "                    <h4>Be roast-aware</h4>\n" +
-    "                    <p>\n" +
-    "                        It may be that the most avid coffee drinkers choose to toast their beans on their own, but I believe that most coffee lovers buy their beans already roasted.\n" +
-    "                        In this case it is very important to pay attention to the roasting date: from that moment, the coffee slowly begins to lose its properties,\n" +
-    "                        for all the reasons we have seen so far.\n" +
-    "                        Generally, as we have already said,\n" +
-    "                    </p>\n" +
-    "                    <span>\n" +
+    "    <p>\n" +
+    "        In fact, the continuous change of temperature when taking the coffee and putting it back in the cold, causes the appearance of condensation,\n" +
+    "        which is absorbed by the coffee, ruining the aromas.\n" +
+    "    </p>\n" +
+    "    <h4>Be roast-aware</h4>\n" +
+    "            <img alt=\"grinding coffee\" loading=\"lazy\" src=\"https://drive.google.com/uc?id=1jSB0tEAV58W4NCPex6oXKCiB4R3GlwCP\"/>\n" +
+    "    <p>\n" +
+    "        It may be that the most avid coffee drinkers choose to toast their beans on their own, but I believe that most coffee lovers buy their beans already roasted.\n" +
+    "        In this case it is very important to pay attention to the roasting date: from that moment, the coffee slowly begins to lose its properties,\n" +
+    "        for all the reasons we have seen so far.\n" +
+    "        Generally, as we have already said,\n" +
+    "    </p>\n" +
+    "    <span id=\"center-span\">\n" +
     "                        whole beans retain their intense aromas and flavors for about a month after roasting, while ground coffee for at most two weeks\n" +
     "                    </span>\n" +
-    "                    <p>\n" +
-    "                        When we buy our coffee, we can find the roasting date on the package,\n" +
-    "                        and we must therefore be careful that it is not too much in the past, otherwise we risk that our coffee quickly loses its characteristics.\n" +
-    "                    </p>\n" +
-    "                    <h4>To Sum Up</h4>\n" +
-    "                    <p>\n" +
-    "                        we have learned that storing coffee to preserve its properties longer requires a lot of attention,\n" +
-    "                        but we can summarize in a few small precautions the secret to always having excellent coffee:\n" +
-    "                        <ul className=\"sum-up\">\n" +
-    "                            <li>Try to buy small quantities of whole beans frequently and grind your own coffee just before using it</li>\n" +
-    "                            <li>Check the roasting date: from that moment the coffee slowly begins to lose its properties</li>\n" +
-    "                            <li>Keep the coffee in a cool, dry and dark place, avoiding contact with the air as much as possible</li>\n" +
-    "                        </ul>\n" +
-    "                    </p>\n"
+    "    <p>\n" +
+    "        When we buy our coffee, we can find the roasting date on the package,\n" +
+    "        and we must therefore be careful that it is not too much in the past, otherwise we risk that our coffee quickly loses its characteristics.\n" +
+    "    </p>\n" +
+    "    <h4>To Sum Up</h4>\n" +
+    "    <p>\n" +
+    "        we have learned that storing coffee to preserve its properties longer requires a lot of attention,\n" +
+    "        but we can summarize in a few small precautions the secret to always having excellent coffee:\n" +
+    "        <ul className=\"sum-up\">\n" +
+    "            <li>Try to buy small quantities of whole beans frequently and grind your own coffee just before using it</li>\n" +
+    "            <li>Check the roasting date: from that moment the coffee slowly begins to lose its properties</li>\n" +
+    "            <li>Keep the coffee in a cool, dry and dark place, avoiding contact with the air as much as possible</li>\n" +
+    "        </ul>\n" +
+    "    </p>\n" +
+    "            </>"
 
 
 
